55fc9deff3cfa8b962b162716023f22c5f1fc884
vlkaaakos <v.aakos@stud.uni-obuda.hu>
2024-03-19 08:38:43 +0100

    message modify

---
+++ Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Message
+    {
+        public string Sender {  get; set; }
+        public DateTime SendDate { get; set; }
+        public string SentMessage {  get; set; }
+    }
+}
